8213a67541f7c2163f45e518b18eb89ab291fdd5
RaghavendraGunturi1 <raghavendra@newowl.co>
2025-12-13 21:55:20 +0530

    Update settings.py

--- codeapt_site/settings.py
+++ codeapt_site/settings.py
@@ -35,13 +35,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <--- ADD THIS LINE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <--- ADD THIS LINE
 ]
 
 ROOT_URLCONF = 'codeapt_site.urls'
