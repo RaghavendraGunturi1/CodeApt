a11c0c82b560a0f2e45c9d350e93e48f7d1e61f5
RaghavendraGunturi1 <raghavendra@newowl.co>
2025-12-13 21:44:06 +0530

    Update settings.py

--- codeapt_site/settings.py
+++ codeapt_site/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 # This collects all static files into one folder for production
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
 # Enable WhiteNoise storage
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
