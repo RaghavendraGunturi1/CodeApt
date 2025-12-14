dc27b033a32e8dd690abae074ec18a9736ea5472
RaghavendraGunturi1 <raghavendra@newowl.co>
2025-12-14 22:30:28 +0530

    d

--- codeapt_site/settings.py
+++ codeapt_site/settings.py
@@ -118,12 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
-# This collects all static files into one folder for production
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
 # Enable WhiteNoise storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
