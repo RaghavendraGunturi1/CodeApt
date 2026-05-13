db359f55bc6408a01d82da902b48bab1b447e99c
RaghavendraGunturi1 <raghavendra@newowl.co>
2026-05-13 16:19:57 +0530

    Update urls.py

--- codeapt_site/urls.py
+++ codeapt_site/urls.py
@@ -1,13 +1,17 @@
 import django_rq
 from django.contrib import admin
-from django.urls import path, include  # Import include\
-from core import views  # Import views to access the vision view
+from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('django-rq/', django_rq.urls),
-    path('', include('core.urls')),  # Connect core urls
-    path('accounts/', include('accounts.urls')), # NEW LINE
+
+    # django-rq dashboard
+    path('django-rq/', include(django_rq.urls)),
+
+    # App routes
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
     path('challenges/', include('challenges.urls')),
-    path('assessments/', include('assessments.urls')),  # NEW LINE for assessments app
+    path('assessments/', include('assessments.urls')),
 ]
